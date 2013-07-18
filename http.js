2ee6ea0e47c9d96d83b1ce954b6faabc11265c2e
John Weis <weis.john@gmail.com>
2013-07-18 18:27:12 -0400

    another one bites the dust

---
+++ http.js
@@ -0,0 +1,15 @@
+var http = require('http')
+  , through = require('through')
+  ;
+
+var upper = through(function(buf) {
+    this.queue(buf.toString().toUpperCase());
+});
+
+var server = http.createServer(function(req, res) {
+    if (req.method === "POST") {
+        req.pipe(upper).pipe(res);
+    }d\
+});
+
+server.listen(8000);
